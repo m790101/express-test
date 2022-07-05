bd7798ce1d29fb397422f2abe24cc72838352a96
m790101 <m790101@hotmail.com>
2022-07-05 22:57:36 +0800

    feature:modify page ui

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ const exphbs = require("express-handlebars")
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.static("public"))
 
 app.get("/",(req,res) => {
     res.render("index")
 })
 
+app.get("/about",(req,res) => {
+    res.render("about")
+})
+
+app.get("/profolio",(req,res) => {
+    res.render("profolio")
+})
+
+app.get("/contact",(req,res) => {
+    res.render("contact")
+})
 
 app.listen(3000,() => {
 console.log("app is running on http://locoal:3000 ")
