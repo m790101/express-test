10c22730b1e3373479f2f068dc4c3423626aa22e
m790101 <m790101@hotmail.com>
2022-07-05 22:13:58 +0800

    feat: add handlebars

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const exphbs = require("express-handlebars")
+
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get("/",(req,res) => {
+    res.render("index")
+})
+
+
+app.listen(3000,() => {
+console.log("app is running on http://locoal:3000 ")
+})
